15343b59f06cc306227dd2ba6c22f3ef4fd36af3
Austin Wynn <36213318+All-I-Do-Is-Wynn@users.noreply.github.com>
2018-03-07 13:20:11 -0500

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,15 @@
+
+public class Math {
+
+	public static void main(String[] args) {
+		int a=1, b=1, c=-2;
+  		boolean end;
+  		end = a!=0&&b*b-4*a*c>0;
+  		System.out.println(end);
+  		
+  		System.out.println((double)5/2 + ", " + (double)(5/2));
+  		
+
+	}
+
+}
